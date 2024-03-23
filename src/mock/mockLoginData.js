3528cceca8fa805c580bef3c3f7fc6928c16abde
daiqian <cuit980@gmail.com>
2024-03-23 18:38:36 -0500

    Update mockLoginData.js

--- src/mock/mockLoginData.js
+++ src/mock/mockLoginData.js
@@ -1,20 +1,16 @@
 const mockLogin = {
+
     username: "validuser",
     password: "validpassword"
-};
+
+}
 
 const mockLoginBadUsername = {
     username: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
     password: "aaaaaaaaaaaaaaaaaaaaaaaaa"
-};
+}
 
 const mockLoginBadPassword = {
     username: "aaaaaaaaaaa",
     password: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-};
-
-module.exports = {
-    mockLogin,
-    mockLoginBadUsername,
-    mockLoginBadUsername
-};
+}
