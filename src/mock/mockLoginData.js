0f3d2123f8009924c4d13fada3a1d98e7ff6d7b4
joshu <joshuakponor@gmail.com>
2024-03-22 23:14:21 -0500

    did the exports

--- src/mock/mockLoginData.js
+++ src/mock/mockLoginData.js
@@ -1,16 +1,20 @@
 const mockLogin = {
-
     username: "validuser",
     password: "validpassword"
-
-}
+};
 
 const mockLoginBadUsername = {
     username: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
     password: "aaaaaaaaaaaaaaaaaaaaaaaaa"
-}
+};
 
 const mockLoginBadPassword = {
     username: "aaaaaaaaaaa",
     password: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-}
+};
+
+module.exports = {
+    mockLogin,
+    mockLoginBadUsername,
+    mockLoginBadUsername
+};
